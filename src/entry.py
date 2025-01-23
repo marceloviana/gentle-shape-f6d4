8e4e6eb025bd66c61fd3ebd33f6f772ff66ed216
Marcelo Viana <marceloviana@macbook.local>
2025-01-23 14:14:11 -0300

    added github actions

--- src/entry.py
+++ src/entry.py
@@ -1,4 +1,4 @@
 from js import Response
 
 async def on_fetch(request, env):
-    return Response.new(f"Hello World - Marcelo Viana teste! {env.MY_VARIABLE} {env.OTHER_VARIABLE or None}")
+    return Response.new(f" {env.MY_VARIABLE} {env.OTHER_VARIABLE or None} sucesso! ")
