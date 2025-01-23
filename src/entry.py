1f56e03fa897c434c0e57f9fd6ec5647222b037b
Marcelo Viana <marceloviana@macbook.local>
2025-01-23 15:01:41 -0300

    added github actions

--- src/entry.py
+++ src/entry.py
@@ -1,4 +1,4 @@
 from js import Response
 
 async def on_fetch(request, env):
-    return Response.new(f" {env.MY_VARIABLE} {env.OTHER_VARIABLE or None} sucesso! ")
+    return Response.new(f" sucesso! ")
